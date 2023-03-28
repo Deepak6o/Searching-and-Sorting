a79ef509e1d711e52ccdf14c47b0e0a96cc79b34
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-03-28 11:24:53 +0530

    Create Permutations in array.cpp

---
+++ Permutations in array.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool isPossible(long long a[], long long b[], int n, long long k) {
+        // Your code goes here
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]+b[i]>=k)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        int n;
+        long long k;
+        cin >> n >> k;
+        long long a[n + 2], b[n + 2];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        for (int i = 0; i < n; i++) cin >> b[i];
+        Solution ob;
+        cout << ob.isPossible(a, b, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
