2143fe809674239a1db72b4bdec68264c4e89b32
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2024-01-01 23:19:12 +0530

    Update Ceil The Floo.cpp

--- Ceil The Floo.cpp
+++ Ceil The Floo.cpp
@@ -1,3 +1,41 @@
+int getceil(vector<int> &a, int n, int x){
+    int l=0;
+    int h=n-1;
+    int ans=-1;
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(a[mid]>=x){
+            ans=a[mid];
+            h=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    return ans;
+}
+int getfloor(vector<int> &a, int n, int x)
+{
+    int l=0;
+    int h=n-1;
+    int ans=-1;
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(a[mid]<=x){
+            ans=a[mid];
+            l=mid+1;
+        }
+        else{
+            h=mid-1;
+        }
+    }
+    return ans;
+}
+pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
+	// Write your code here.
+    pair<int,int> ans(getfloor(a,n,x),getceil(a,n,x));
+    return ans;
+}
 pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
 	// Write your code here.
     // pair<int,int> ans;
